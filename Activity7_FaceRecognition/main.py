dca41753b366db6c834c947aa73193d1f6109242
kresanz <116730290+kresanz@users.noreply.github.com>
2024-02-26 06:39:36 +0800

    Added Video File Face Detection

--- Activity7_FaceRecognition/main.py
+++ Activity7_FaceRecognition/main.py
@@ -15,19 +15,53 @@ def clear_folder(folder):
         if os.path.isfile(file_path):
             os.remove(file_path)
 
-def detect(folder_path, save_folder):
+def scan_files(folder_path, save_folder):
     """"Takes the roi of the image and saves it"""
     clear_folder(save_folder)
-    face_cascade = cv2.CascadeClassifier('Activity7_FaceRecognition/haarcascade_frontalface_default.xml')
-    
     image_files = []
+    video_files = []
     for file_name in os.listdir(folder_path):
         if file_name.endswith(('.png', '.jpg', '.jpeg')):
             image_files.append(os.path.join(folder_path, file_name))
+        elif file_name.endswith(('.mp4','.mkv')):
+            video_files.append(os.path.join(folder_path, file_name))
+    image_detect(image_files, save_folder)
+    video_detect(video_files, save_folder)
+
+def video_detect(video_files, save_folder):
+    print("Detecting video")
+    face_cascade = cv2.CascadeClassifier('Activity7_FaceRecognition/haarcascade_frontalface_default.xml')
+    
+    j = 1 #counter of saved images
+    
+    for file in video_files:
+        video = cv2.VideoCapture(file)
+        if not video.isOpened():
+            print(f"Error: Could not open video file {file}")
+            continue
+
+        while True:
+            ret, frame = video.read()
+            if not ret:
+                break
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            for (x, y, w, h) in faces:
+                roi_gray = gray[y:y + h, x:x + w]
+                cv2.imwrite(os.path.join(save_folder, f'image_{j}.jpg'), roi_gray)
+                j += 1
+        video.release()
+
+def image_detect(image_files, save_folder):
+    print("Detecting images")
+    face_cascade = cv2.CascadeClassifier('Activity7_FaceRecognition/haarcascade_frontalface_default.xml')
     
     i = 1 #counter of input images
     j = 1 #counter of saved images
     n = len(image_files)
+    if n==0: return
     while True:
         print(i)
         image = cv2.imread(image_files[i-1])
@@ -49,6 +83,7 @@ if __name__ == "__main__":
     root = 'Activity7_FaceRecognition/images/'
     students = [['Marie','Hernandez'],['Kenneth','Estacion'],
                 ['Ed','Figueroa'],['Jericho','Ecubin']]
-    for (firstname, lastname) in students:
+    """for (firstname, lastname) in students:
       print(lastname)
-      detect(root+firstname,root+lastname)
+      scan_files(root+firstname,root+lastname)"""
+    
